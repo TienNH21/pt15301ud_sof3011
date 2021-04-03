d329ef5b6cd32dde634ccc2749d9dc289aaa6349
HoangTien339 <tiennguyenhoang339@gmail.com>
2021-04-03 12:18:21 +0700

    2nd commit

--- src/com/dao/UserDAO.java
+++ src/com/dao/UserDAO.java
@@ -1,5 +1,9 @@
 package com.dao;
 
+import java.util.List;
+
+import javax.persistence.Query;
+
 import org.hibernate.Session;
 
 import com.entity.User;
@@ -17,5 +21,43 @@ public class UserDAO {
 		entity.setId(id);
 		
 		return entity;
+	}
+	
+	public List<User> paginate(int offset, int limit)
+	{
+		String hql = "FROM User";
+		Session session = HibernateUtils.getSession();
+		Query query = session.createQuery(hql);
+		query.setFirstResult(offset);
+		query.setMaxResults(offset + limit);
+		
+		List<User> listUser = query.getResultList();
+		
+		return listUser;
+	}
+	
+	public User findById(int id)
+	{
+		Session session = HibernateUtils.getSession();
+		User entity = session.get(User.class, id);
+		
+		return entity;
+	}
+	
+	public void update(User entity)
+	{
+		Session session = HibernateUtils.getSession();
+		
+		try {
+			session.beginTransaction();
+
+			session.clear();
+			session.update(entity);
+			
+			session.getTransaction().commit();
+		} catch (Exception e) {
+			e.printStackTrace();
+			session.getTransaction().rollback();
+		}
 	}
 }
---
+++ src/com/servlet/admin/UserServlet.java
@@ -0,0 +1,44 @@
+package com.servlet.admin;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.dao.UserDAO;
+import com.entity.User;
+
+@WebServlet("/admin/users")
+public class UserServlet extends HttpServlet {
+	private UserDAO userDAO;
+	
+	public UserServlet()
+	{
+		super();
+		
+		this.userDAO = new UserDAO();
+	}
+	
+	@Override
+	protected void doGet(
+		HttpServletRequest request,
+		HttpServletResponse response
+	) throws ServletException, IOException {
+		String pageStr = request.getParameter("page"),
+				limitStr = request.getParameter("limit");
+		int page = pageStr == null ? 1 : Integer.parseInt( pageStr ),
+			limit = limitStr == null ? 10 : Integer.parseInt( limitStr ),
+			offset = limit * (page - 1);
+		List<User> listUser = this.userDAO.paginate(offset, limit);
+
+		request.setAttribute("page", page);
+		request.setAttribute("listUser", listUser);
+
+		request.getRequestDispatcher("/views/admin/users/index.jsp")
+		.forward(request, response);
+	}
+}
